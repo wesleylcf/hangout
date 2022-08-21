8ad9e759bf34e710d8e1026e2ba021f757f8dd2b
wesley lim <wesleylim.work@gmail.com>
2022-08-21 20:44:27 +0800

    (fix): Hash passwords when seeding data

--- sc2006-backend/src/auth/auth.service.ts
+++ sc2006-backend/src/auth/auth.service.ts
@@ -7,59 +7,68 @@ import { ValidateUserOutcome } from '../constants/auth';
 
 @Injectable()
 export class AuthService {
-  private readonly saltRounds = 10;
-  constructor(
-    private userService: UserService,
-    private jwtService: JwtService,
-    private logger: Logger,
-  ) {}
+	private readonly saltRounds = 10;
+	constructor(
+		private userService: UserService,
+		private jwtService: JwtService,
+		private logger: Logger,
+	) {}
 
-  async signup({ username, password }: AuthUserDto) {
-    const storedUser = await this.userService.findOne(username);
-    if (storedUser) {
-      return { error: 'Unable to sign up user: User already exists' };
-    }
-    try {
-      const hashedPassword = await bcrypt.hash(password, this.saltRounds);
-      await this.userService.create({
-        username: username,
-        password: hashedPassword,
-      });
-      return { error: null };
-    } catch (e) {
-      return { error: e.message };
-    }
-  }
+	async signup({ username, password }: AuthUserDto) {
+		const storedUser = await this.userService.findOne(username);
+		if (storedUser) {
+			return { error: 'Unable to sign up user: User already exists' };
+		}
+		try {
+			const hashedPassword = await bcrypt.hash(password, this.saltRounds);
+			await this.userService.create({
+				username: username,
+				password: hashedPassword,
+			});
+			return { error: null };
+		} catch (e) {
+			return { error: e.message };
+		}
+	}
 
-  async login(user: ValidateUserOutcome) {
-    return user['error']
-      ? { error: 'Invalid username or password' }
-      : {
-          access_token: this.jwtService.sign(user),
-        };
-  }
+	async login(user: ValidateUserOutcome) {
+		return user['error']
+			? { error: 'Invalid username or password' }
+			: {
+					access_token: this.jwtService.sign(user),
+					user,
+			  };
+	}
 
-  async validateUser(
-    username: string,
-    inputPassword: string,
-  ): Promise<ValidateUserOutcome> {
-    try {
-      const user = await this.userService.findOne(username);
-      if (!user) {
-        throw new Error(`User ${username} not found`);
-      }
-      const response: boolean = await bcrypt.compare(
-        inputPassword,
-        user.password,
-      );
-      if (!response) {
-        throw new Error('Invalid password');
-      }
+	async validateUser(
+		username: string,
+		inputPassword: string,
+	): Promise<ValidateUserOutcome> {
+		try {
+			const user = await this.userService.findOne(username);
+			if (!user) {
+				throw new Error(`User ${username} not found`);
+			}
+			const response: boolean = await bcrypt.compare(
+				inputPassword,
+				user.password,
+			);
+			if (!response) {
+				throw new Error('Invalid password');
+			}
 
-      return user;
-    } catch (e) {
-      this.logger.warn(`User validation failed: ${e.message}`, 'AuthService');
-      return { error: e.message };
-    }
-  }
+			return user;
+		} catch (e) {
+			this.logger.warn(`User validation failed: ${e.message}`, 'AuthService');
+			return { error: e.message };
+		}
+	}
+
+	async hashPasswords(users: { password: string }[]) {
+		return Promise.all(
+			users.map(async (user) => {
+				return await bcrypt.hash(user.password, 10);
+			}),
+		);
+	}
 }
