3098c19fc2d57c8ec66aaaa0d2773876a12564d6
anhngoc <ngoc2162003@gmail.com>
2022-11-01 02:06:08 +0800

    (style): Edit forms and logo

--- sc2006-web/containers/app/Logo.tsx
+++ sc2006-web/containers/app/Logo.tsx
@@ -3,7 +3,7 @@ import { ShareAltOutlined } from '@ant-design/icons';
 
 export function Logo() {
 	return (
-		<div className="flex justify-start w-2/6 items-center px-5 text-black">
+		<div className="flex justify-start w-1/2 items-center px-10 text-black font-mono">
 			<ShareAltOutlined className="pr-1" />
 			Hangout!
 		</div>
