c7f552e8d5e4906c0102bb1ff3cf60a99aa32e43
wesley lim <wesleylim.work@gmail.com>
2022-11-01 11:49:40 +0800

    (style): Minor updates

--- sc2006-web/containers/home/demo.tsx
+++ sc2006-web/containers/home/demo.tsx
@@ -6,26 +6,29 @@ import Image from 'next/image';
 export const Demo = () => {
 	return (
 		<div className="h-full w-full space-y-2">
-			<section className="flex flex-col items-center">
-				<h1 className="text-7xl text-gray-700 left-1/2 font-mono">
-					Hangout!
-				</h1>
-				<span className="w-full p-0.5 bg-blue-400 lg:w-1/3"></span>
-			</section>
-			<section className="flex flex-row h-full items-center space-y-8 w-11/12 mx-auto">
-				<div className="w-1/2 h-3/5 flex flex-row items-center justify-center relative">
-					<Image src="/hangout.png" layout="fill" />
-				</div>
-				<div className="w-1/2 h-3/5 flex flex-col justify-center">
-					<h1 className="text-5xl text-pink-400">
-						Planning outings made simple.
+			<div className="h-screen w-full">
+				<section className="flex flex-col items-center h-1/6 justify-center">
+					<h1 className="text-7xl text-gray-700 left-1/2 font-mono">
+						Hangout!
 					</h1>
-					<p className="text-2xl">
-						Find the place that best suits everyone&#39;s schedule, preferences,
-						and is equidistant from everyone!{' '}
-					</p>
-				</div>
-			</section>
+					<span className="w-full p-0.5 bg-blue-400 lg:w-1/3"></span>
+				</section>
+				<section className="flex flex-row h-5/6 items-center space-y-8 w-11/12 mx-auto">
+					<div className="w-1/2 h-3/5 flex flex-row items-center justify-center relative">
+						<Image src="/hangout.png" layout="fill" />
+					</div>
+					<div className="w-1/2 h-3/5 flex flex-col justify-center">
+						<h1 className="text-5xl text-pink-400">
+							Planning outings made simple.
+						</h1>
+						<p className="text-2xl">
+							Find the place that best suits everyone&#39;s schedule,
+							preferences, and is equidistant from everyone!{' '}
+						</p>
+					</div>
+				</section>
+			</div>
+
 			<section className="flex flex-col space-y-8 items-center bg-gray-50">
 				<h1 className="text-4xl text-sky-400 left-1/2 mt-8">
 					Here&#39;s how it works
@@ -42,8 +45,8 @@ export const Demo = () => {
 							<p className="text-xl">Tell us your preferences</p>
 							<p>
 								We will suggest 5 places for each of your common preference.
-								Note that the app will complain if there are no
-								common preferences among the participants!
+								Note that the app will complain if there are no common
+								preferences among the participants!
 							</p>
 						</Timeline.Item>
 						<Timeline.Item color="green" style={{ paddingBottom: '3rem' }}>
@@ -56,9 +59,8 @@ export const Demo = () => {
 						<Timeline.Item color="green" style={{ paddingBottom: '3rem' }}>
 							<p className="text-xl">Tell us your address</p>
 							<p>
-								We will calculate the central point from all
-								participants&#39; addresses, then suggest places closest to that
-								point.
+								We will calculate the central point from all participants&#39;
+								addresses, then suggest places closest to that point.
 							</p>
 						</Timeline.Item>
 						<Timeline.Item color="green" style={{ paddingBottom: '3rem' }}>
