9e160b67a723ebe062336e52f9c719b9a1e0700a
wesley lim <wesleylim.work@gmail.com>
2022-12-30 16:30:43 +0800

    (feat): Add firestore-types package

--- sc2006-common/src/constants/EventResult.ts
+++ sc2006-common/src/constants/EventResult.ts
@@ -1,4 +1,4 @@
-import { Timestamp } from 'firebase/firestore';
+import { Timestamp } from '@firebase/firestore-types';
 
 export interface DbEventResultRes extends Omit<DbEventResult, 'createdAt'> {
 	uuid: string;
